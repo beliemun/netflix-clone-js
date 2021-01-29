ba6680449cc20fa12be743340bdf53f12eae121c
beliemun <beliemun@paran.com>
2021-01-29 09:43:45 +0900

    update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,29 +1,55 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  width: 100%;
+  height: 50px;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.8);
+  font-size: 16px;
+`;
 
 const List = styled.ul`
   display: flex;
 `;
 
-const StyledLink = styled(Link)``;
+const Item = styled.li`
+  width: 100px;
+  text-align: center;
+  border-bottom: 3px solid
+    ${(props) => (props.current ? "#e74c3c" : "transparent")};
+  transition: border-bottom 0.3s ease-in-out;
+`;
 
-const Header = () => {
+const StyledLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Header = ({ location: { pathname } }) => {
   return (
-    <header>
+    <StyledHeader>
       <List>
-        <li>
-          <StyledLink to="/#">Moives</StyledLink>
-        </li>
-        <li>
-          <StyledLink to="/#/tv">TV</StyledLink>
-        </li>
-        <li>
-          <StyledLink to="/#/search">Search</StyledLink>
-        </li>
+        <Item current={pathname === "/"}>
+          <StyledLink to="/">Moives</StyledLink>
+        </Item>
+        <Item current={pathname === "/tv"}>
+          <StyledLink to="/tv">TV</StyledLink>
+        </Item>
+        <Item current={pathname === "/search"}>
+          <StyledLink to="/search">Search</StyledLink>
+        </Item>
       </List>
-    </header>
+    </StyledHeader>
   );
 };
 
-export default Header;
+export default withRouter(Header);
