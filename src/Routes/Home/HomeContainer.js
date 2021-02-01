53fac53cc255d618144c8b2ef6eda7acbca6ba91
beliemun <beliemun@paran.com>
2021-02-01 14:15:07 +0900

    update

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,14 +1,42 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesAPI } from "api";
 
 class HomeContainer extends React.Component {
   state = {
     nowPlaying: null,
     upComing: null,
     popular: null,
-    loading: null,
+    loading: true,
     error: null,
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesAPI.nowPlaying();
+      const {
+        data: { results: upComing },
+      } = await moviesAPI.upComing();
+      const {
+        data: { results: popular },
+      } = await moviesAPI.popular();
+      this.setState({
+        nowPlaying,
+        upComing,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find moives information.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
 
   render() {
     const { nowPlaying, upComing, popular, loading, error } = this.state;
