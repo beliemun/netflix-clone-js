0fde408d9e8da92960ba0202ba970582f5894bc3
beliemun <beliemun@paran.com>
2021-02-01 23:21:47 +0900

    update

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import TVPresenter from "./TVPresenter";
+import { tvAPI } from "api";
 
 const TVContainer = () => {
   const [topRated, setTopRated] = useState(null);
@@ -7,6 +8,25 @@ const TVContainer = () => {
   const [airingToday, setAiringToday] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadDatas();
+  }, [])
+
+  const loadDatas = async () => {
+    try {
+      const { data: { results: topRated } } = await tvAPI.topRated();
+      const { data: { results: popular } } = await tvAPI.popular();
+      const { data: { results: airingToday } } = await tvAPI.airingToday();
+      setTopRated(topRated);
+      setPopular(popular);
+      setAiringToday(airingToday);
+    } catch {
+      setError("Can't find TV information.");
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <TVPresenter
